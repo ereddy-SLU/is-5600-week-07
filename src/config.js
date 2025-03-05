16179a9f691a2d23089195d043b6bef5e96c7977
ereddy-SLU <ereddy1@slu.edu>
2025-03-05 02:54:44 +0000

    Lab-07-Assignemnet-Submission

--- src/config.js
+++ src/config.js
@@ -1,2 +1,1 @@
-// TODO: Replace this with your own Server URL for your codespace
-export const BASE_URL = "https://glowing-space-memory-q7p4jvr9g4p424rj7-3080.app.github.dev";
+export const BASE_URL = "https://expert-memory-wr5x7jpwrg6rfv9v4-3080.app.github.dev";
